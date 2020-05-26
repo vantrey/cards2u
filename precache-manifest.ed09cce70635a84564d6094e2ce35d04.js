c5c620a1c3d49441338c8e56acde15ed4d094dd2
Ivan Tolkachev <vantrey@yandex.ru>
2020-05-26 09:45:03 +0300

    Updates

---
+++ precache-manifest.ed09cce70635a84564d6094e2ce35d04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17b3528598a473d6390f2e46f145aa0f",
+    "url": "/cards2u/index.html"
+  },
+  {
+    "revision": "8b0e629f2963fb644e8a",
+    "url": "/cards2u/static/css/main.d20c0a68.chunk.css"
+  },
+  {
+    "revision": "dbab76cba83f01e43364",
+    "url": "/cards2u/static/js/2.d0b8e078.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cards2u/static/js/2.d0b8e078.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b0e629f2963fb644e8a",
+    "url": "/cards2u/static/js/main.6dfbb7cc.chunk.js"
+  },
+  {
+    "revision": "40294b5d506c4a502201",
+    "url": "/cards2u/static/js/runtime-main.f20b49e5.js"
+  }
+]);
