7b5d17eda48eb4f9c1423d785bba15f869ab70f3
Ivan Tolkachev <vantrey@yandex.ru>
2020-05-30 20:57:33 +0300

    add api with types, add useType

---
+++ src/main/types/entities.ts
@@ -0,0 +1,11 @@
+export type UserType = {
+  avatar: string
+  created: string
+  email: string
+  isAdmin: boolean
+  name: string
+  publicCardPacksCount: number
+  updated: string
+  verified: boolean
+  _id: string
+}
