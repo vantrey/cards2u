722510f34b9b2caabc6a978aef5dcc482634a504
Ivan Tolkachev <vantrey@yandex.ru>
2020-05-31 14:03:48 +0300

    add form to Registration.tsx

--- src/main/auth/registration/Regirtration.tsx
+++ src/main/auth/registration/Regirtration.tsx
@@ -1,10 +1,47 @@
 import React from 'react';
 import styles from './Registration.module.css'
+import * as yup from "yup";
+import {useForm} from "react-hook-form";
+import Input from "../../ui/common/Input/Input";
+import Button from "../../ui/common/Button/Button";
 
 const Registration = () => {
-
+  const registrationFormSchema = yup.object().shape({
+    email: yup.string().required('⚠ please, fill up your email')
+      .email('⚠ please, fill up a valid email address'),
+    password: yup.string().required('⚠ please, fill up your password'),
+    passwordConfirmation: yup.string()
+      .oneOf([yup.ref('password'), undefined], 'password mismatch')
+  })
+  const {register, handleSubmit, errors} = useForm({mode: 'onBlur', validationSchema: registrationFormSchema});
+  const onSubmit = (data: any, e: any) => {
+    e.target.reset()
+    console.log(data)
+  }
   return  <div className={styles.registration}>
-REGISTRATION
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Input
+        name='email'
+        register={register}
+        error={errors.email}
+        placeholder='email'
+      />
+      <Input
+        type='password'
+        name='password'
+        register={register}
+        error={errors.password}
+        placeholder='password'
+      />
+      <Input
+        type='password'
+        name='passwordConfirmation'
+        register={register}
+        error={errors.passwordConfirmation}
+        placeholder='confirm password'
+      />
+      <Button tittle='sign up free'/>
+    </form>
   </div>
 }
 export default Registration
