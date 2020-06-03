1d41d06f2155bfcfd54ae9aab9d18e4622407a92
ZdraviiSmisl <55047650+ZdraviiSmisl@users.noreply.github.com>
2020-06-03 14:19:45 +0300

    restorePsw form container,page,module.css,formShema,reducer

---
+++ src/main/auth/restorePsw/RestorePswPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import RestorePswContainer from "./RestorePswContainer";
+
+const RestorePswPage:React.FC = () => {
+    return <RestorePswContainer/>
+};
+
+export default RestorePswPage;
--- src/main/ui/components/routes/Routes.tsx
+++ src/main/ui/components/routes/Routes.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
 import Login from "../../../auth/login/Login";
-import Registration from "../../../auth/registration/Regirtration";
-import RestorePsw from "../../../auth/restorePsw/RestorePsw";
 import NewPsw from "../../../auth/newPsw/newPsw";
 import Profile from "../../../auth/profile/Profile";
-import RegistrationContainer from "../../../auth/registration/RegirtrationContainer";
 import RegistrationPage from '../../../auth/registration/RegirtrationPage';
+import RestorePswPage from "../../../auth/restorePsw/RestorePswPage";
 
 export const LOGIN_PATH = '/login'
 export const REGISTRATION_PATH = '/registration'
@@ -15,12 +13,12 @@ export const NEW_PSW_PATH = '/newPsw'
 export const PROFILE_PATH = '/profile'
 
 const Routs = () => {
-  return <div>
-    <Route path={LOGIN_PATH} render={() => <Login/>}/>
-    <Route path={REGISTRATION_PATH} render={() => <RegistrationPage/>}/>
-    <Route path={RESTORE_PSW_PATH} render={() => <RestorePsw/>}/>
-    <Route path={NEW_PSW_PATH} render={() => <NewPsw/>}/>
-    <Route path={PROFILE_PATH} render={() => <Profile/>}/>
-  </div>
+    return <div>
+        <Route path={LOGIN_PATH} render={() => <Login/>}/>
+        <Route path={REGISTRATION_PATH} render={() => <RegistrationPage/>}/>
+        <Route path={RESTORE_PSW_PATH} render={() => <RestorePswPage/>}/>
+        <Route path={NEW_PSW_PATH} render={() => <NewPsw/>}/>
+        <Route path={PROFILE_PATH} render={() => <Profile/>}/>
+    </div>
 }
 export default Routs
