6f2bd4f9fcb5ac5d1e79dde6bf251932474b8d8d
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2020-06-05 16:14:19 +0400

    added code for close form-content

--- src/main/ui/common/scroll/Scroll.jsx
+++ src/main/ui/common/scroll/Scroll.jsx
@@ -2,23 +2,29 @@ import React from 'react';
 import styles from './Scroll.module.css';
 import scrollBg from '../../images/scroll.png';
 import closeIcon from '../../icons/cancel .png';
+import Loader from "../loader/Loader";
 
 
 const Scroll = ({ modal, setModal }) => {
 
-	const closeModal = () =>{
-		setModal(false);
+	const closeModal = (e) =>{
+		if (e.target.matches('#closeIconId') || (e.target.closest('#skrollContent') === null) ) {
+			setModal(false);
+		}
 	};
 
 	const classForModal = modal === true ? `${styles.scroll__wrap} ${styles.scroll__wrap_active}` : `${styles.scroll__wrap}`;
 
 	return (
-		<div className={classForModal} id='modal'>
-			<div className={styles.scroll__img}>
-				<div className={styles.scroll__icon} id='closeIconId' onClick={closeModal}>
-					<img src={closeIcon} alt="cancel-Icon"/>
+		<div className={classForModal}  onClick={closeModal}>
+			<div className={styles.scroll__img}  id='skrollContent'>
+				<div className={styles.scroll__icon}  >
+					<img src={closeIcon} alt="cancel-Icon" id='closeIconId'/>
 				</div>
 				<img src={scrollBg} alt="scroll"/>
+				<div className={styles.scroll__loader}>
+					<Loader/>
+				</div>
 			</div>
 		</div>
 	)
