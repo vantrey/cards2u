bee51a7ede434073fc0e28584b6e7debd09fc179
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2020-06-05 15:23:35 +0400

    anima-n for icons sound + skip

--- src/main/ui/components/intro/Intro.jsx
+++ src/main/ui/components/intro/Intro.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import styles from './Intro.module.css'
 import soundOf from '../../icons/sound-of.svg'
 import soundOn from '../../icons/sound-on.svg'
-// import skip from '../../icons/skip.svg'
 import skip from '../../icons/skip-circle .svg'
 import intro from '../../video/Intro-53-commpres.mp4'
 import intro_webm from '../../video/Intro-53-commpres-webm.webm'
