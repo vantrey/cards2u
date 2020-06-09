8c9466ddaeb3b80b0f275a2453cfbf6cde85a9df
natasha17-spec <472682289popsik>
2020-06-09 09:58:08 +0300

    add tokenDeathTime, comparison tokenTime

--- src/main/helpers/repos_localStorrage/Token.ts
+++ src/main/helpers/repos_localStorrage/Token.ts
@@ -7,13 +7,12 @@ export const repository = {
         localStorage.setItem("token", stateAsString);
     },
     getToken() {
-        debugger
         let getTokenFromLS: any = localStorage.getItem("token");
         if (getTokenFromLS != null) {
             getTokenFromLS = JSON.parse(getTokenFromLS);
-            let dateToken = new Date().getMilliseconds()
-            if (getTokenFromLS.tokenDeathTime<dateToken) {
-                console.log(getTokenFromLS)
+            let dateToken = new Date().getTime()
+            if (getTokenFromLS.tokenDeathTime>dateToken) {
+                console.log("Token is valid")
                 return getTokenFromLS.token
             }
         }
