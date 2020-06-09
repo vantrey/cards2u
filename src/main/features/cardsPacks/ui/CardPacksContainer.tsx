26737c71d27692b09b9bf42a34959c2430bc6ed7
Ivan Tolkachev <vantrey@yandex.ru>
2020-06-09 13:31:06 +0300

    add lodash sort

--- src/main/features/cardsPacks/ui/CardPacksContainer.tsx
+++ src/main/features/cardsPacks/ui/CardPacksContainer.tsx
@@ -1,22 +1,36 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import styles from './CardPacks.module.css'
 import {useDispatch, useSelector} from "react-redux";
 import {AppStateType} from "../../../bll/store/store";
 import {getCardPacks} from "../bll/cardPacksReducer";
 import {repository} from "../../../helpers/repos_localStorrage/Token";
+import lodash from 'lodash'
+import {CardPackType} from "../../../types/entities";
 
 const CardPacksContainer = () => {
-  const {cardPacks} = useSelector((state: AppStateType)=>state.cardPacks)
+  const {cardPacks} = useSelector((state: AppStateType) => state.cardPacks)
+  const [cardPacksOrdered, setCardPacksOrdered] = useState<Array<CardPackType>>([])
   const dispatch = useDispatch()
   const token = repository.getToken()
   useEffect(() => {
     dispatch(getCardPacks(token))
   }, [])
+  useEffect(() => {
+    setCardPacksOrdered(cardPacks)
+  }, [cardPacks])
+  const onSortClickUp = () => {
+    const Ordered = lodash.orderBy(cardPacks, 'name', 'asc')
+    setCardPacksOrdered(Ordered)
+  }
+  const onSortClickDown = () => {
+    const Ordered = lodash.orderBy(cardPacks, 'name', 'desc')
+    setCardPacksOrdered(Ordered)
+  }
   return (
     <div className={styles.cardPacks}>
       <div className={styles.row}>
-        <h3>Name</h3>
-        {cardPacks.map(p => {
+        <h3>Name <button onClick={onSortClickUp}>&#8593;</button><button onClick={onSortClickDown}>&#8595;</button></h3>
+        {cardPacksOrdered.map(p => {
           return (
             <div key={p._id}>{p.name}</div>
           )
@@ -24,7 +38,7 @@ const CardPacksContainer = () => {
       </div>
       <div className={styles.row}>
         <h3>Grade</h3>
-        {cardPacks.map(p => {
+        {cardPacksOrdered.map(p => {
           return (
             <div key={p._id}>{p.grade}</div>
           )
