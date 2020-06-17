8f2a4eabf1b498366e6a302e4d4903d6499cc28b
ZdraviiSmisl <55047650+ZdraviiSmisl@users.noreply.github.com>
2020-06-17 13:47:06 +0300

    add react-pagination for cards

--- src/main/features/Cards/bll/cardsReducer.ts
+++ src/main/features/Cards/bll/cardsReducer.ts
@@ -9,7 +9,7 @@ const initialState = {
     cards: [] as Array<CardType>,
     isFetching: false,
     isSuccess: false,
-    page:1,
+    page: 1,
     pageCount: 10,
     cardsTotalCount: 0
 
@@ -36,11 +36,16 @@ export const CardsReducer = (state: initialStateType = initialState, action: Act
                 ...state,
                 isSuccess: action.isSuccess
             }
-     /*       case  "CARDS_REDUCER/SET_CARDS_DATA":
+        case  "CARDS_REDUCER/SET_CARDS_DATA":
             return {
                 ...state,
-...action.payload
-            }*/
+                ...action.payload
+            }
+        case  "CARDS_REDUCER/SET_PAGE":
+            return {
+                ...state,
+                page: action.page
+            }
 
         default:
             return state;
@@ -53,9 +58,11 @@ const actions = {
     setCards: (cards: Array<CardType>) => ({type: 'CARDS_REDUCER/SET_CARDS', cards} as const),
     set_Fetching: (isFetching: boolean) => ({type: "CARDS_REDUCER/IS_FETCHING", isFetching} as const),
     set_Success: (isSuccess: boolean) => ({type: "CARDS_REDUCER/SET_SUCCESS", isSuccess} as const),
-   /* set_Cards_Count: (cardsTotalCount:number ,page:number,pageCount:number) =>
-        ({type: "CARDS_REDUCER/SET_CARDS_DATA", payload:{cardsTotalCount,page,pageCount}} as const),
-*/
+    set_Cards_Count: (cardsTotalCount: number, page: number, pageCount: number) =>
+        ({type: "CARDS_REDUCER/SET_CARDS_DATA", payload: {cardsTotalCount, page, pageCount}} as const),
+    setPage: (page: number) => ({
+        type: 'CARDS_REDUCER/SET_PAGE', page
+    } as const)
 }
 
 type ActionsType = InferActionTypes<typeof actions>
@@ -66,12 +73,15 @@ type DispatchType = ThunkDispatch<AppStateType, unknown, ActionsType>
 
 export const get_Cards = (cardsPack_id: string): ThunkType =>
     async (dispatch: DispatchType, getState: () => AppStateType) => {
+       /* let page = getState().cards.page;
+        let pageCount = getState().cards.pageCount*/
         try {
             dispatch(actions.set_Fetching(true));
             let token = repository.getToken();
             if (!token) token = ''
             const res = await CardsApi.getCards(cardsPack_id, token);
             dispatch(actions.setCards(res.data.cards));
+            dispatch(actions.set_Cards_Count(res.data.cardsTotalCount, res.data.page, res.data.pageCount));
             dispatch(actions.set_Fetching(false));
             repository.saveToken(res.data.token, res.data.tokenDeathTime);
         } catch (e) {
--- src/main/features/Cards/ui/CardsContainer.tsx
+++ src/main/features/Cards/ui/CardsContainer.tsx
@@ -4,12 +4,22 @@ import {useDispatch, useSelector} from "react-redux";
 import {AppStateType} from "../../../bll/store/store";
 import {add_Card, delete_Card, get_Cards, update_Card} from "../bll/cardsReducer";
 import {useParams} from "react-router-dom";
+import {actions} from "../../../getUser/bll/getUserReducer";
+import ReactPaginate from "react-paginate";
+import styles from './Cards.module.css'
 
 
 const CardsContainer: React.FC = () => {
     const {pack_id} = useParams();
-    const {cards, isFetching} = useSelector((state: AppStateType) => state.cards);
+    const {cards, isFetching,page,pageCount,cardsTotalCount} = useSelector((state: AppStateType) => state.cards);
     const dispatch = useDispatch();
+
+
+    const pageChangedHandler = (page: { selected: number }) => {
+        dispatch(actions.setPage(page.selected + 1))
+    }
+
+
 
     useEffect(() => {
         dispatch(get_Cards(pack_id))
@@ -25,9 +35,27 @@ const CardsContainer: React.FC = () => {
         dispatch(update_Card({_id,answer:'wtf?' },pack_id))
     }
 
-
+    const pageCountSize = Math.ceil(cardsTotalCount / pageCount)
     return (
-        <div>
+        <div className={styles.Cards_container}>
+            <ReactPaginate
+                previousLabel={'<'}
+                nextLabel={'>'}
+                breakLabel={'...'}
+                breakClassName={'break-me'}
+                pageCount={pageCountSize}
+                marginPagesDisplayed={2}
+                pageRangeDisplayed={5}
+                onPageChange={pageChangedHandler}
+                containerClassName={'pagination'}
+                activeClassName={'active'}
+                pageClassName='page-item'
+                pageLinkClassName='page-link'
+                previousClassName='page-item'
+                nextClassName='page-item'
+                previousLinkClassName="page-link"
+                nextLinkClassName="page-link"
+            />
             <Cards
                 isFetching={isFetching}
                 cards={cards}
