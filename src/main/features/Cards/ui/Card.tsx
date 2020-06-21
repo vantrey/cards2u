a429820fd0253503fb55dd9589dbbff9d9c6280b
natasha17-spec <472682289popsik>
2020-06-21 15:05:28 +0300

    add modal window for add pa

--- src/main/features/Cards/ui/Card.tsx
+++ src/main/features/Cards/ui/Card.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {repository} from "../../../helpers/repos_localStorage/Token";
-
+import style from "./Cards.module.css"
 
 type CardPropsType = {
   answer: string
@@ -15,10 +15,16 @@ type CardPropsType = {
 const Card: React.FC<CardPropsType> = ({...props}) => {
   return (
     <>
-      <div>{props.question}</div>
-      <div>{props.answer}</div>
-      <div style={{'color': 'green'}}>grade:{props.grade}</div>
-      <div style={{'color': 'blue'}}>shots:{props.shots}</div>
+        <div className={style.wrap}>
+            <div className={style.card}>
+                <div className={style.front}><span>{props.question}</span></div>
+                <div className={style.back}><span>{props.answer}</span></div>
+            </div>
+        </div>
+        <div className={style.grade_shots}>
+            <div style={{'color': 'green'}}>grade:{props.grade}</div>
+            <div style={{'color': 'blue'}}>shots:{props.shots}</div>
+        </div>
 
       { repository.get_Auth_id() === props.userId &&
       <div>
