da33271d325522ab9566ec7c624a8a78e86ca948
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2020-06-22 02:57:38 +0400

    menu-taverna (added animation)

--- src/main/ui/components/menu/taverna/Taverna.tsx
+++ src/main/ui/components/menu/taverna/Taverna.tsx
@@ -5,15 +5,22 @@ import styles from './Taverna.module.css'
 const Taverna: React.FC = (): any => {
     return (
         <div className={styles.taverna__wrap}>
-            <div className={styles.taverna__left}>left </div>
+            <div className={styles.taverna__left}> </div>
             <div className={styles.taverna__main}>
-                <div className={styles.main__item}>item </div>
-                <div className={styles.main__item}>item </div>
-                <div className={styles.main__item}>item </div>
-                <div className={styles.main__item}>item </div>
-
+                <div className={`${styles.main__item} ${ styles.main__item_pack}`}>
+                   <h1>GAME</h1>
+                </div>
+                <div className={`${styles.main__item} ${ styles.main__item_card}`}>
+                    <h1>CREATE CARDS</h1>
+                </div>
+                <div className={`${styles.main__item} ${ styles.main__item_game}`}>
+                    <h1>FIND A DECK</h1>
+                </div>
+                <div className={`${styles.main__item} ${ styles.main__item_home}`}>
+                    <h1>HOME</h1>
+                </div>
             </div>
-            <div className={styles.taverna__right}>right </div>
+            <div className={styles.taverna__right}> </div>
         </div>
     )
 }
