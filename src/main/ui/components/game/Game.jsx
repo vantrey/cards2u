39208a7c9bc8d7d6a04e377d913b0001c95a9a66
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2020-06-25 18:04:23 +0400

    games -> added decks

--- src/main/ui/components/game/Game.jsx
+++ src/main/ui/components/game/Game.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import styles from './Game.module.css';
+import Deck_blue from "./deck/Deck_blue";
+import Deck_red from "./deck/Deck_red";
+import Deck_green from "./deck/Deck_green";
+import Deck_yellow from "./deck/Deck_yellow";
 
 
 
@@ -14,11 +18,16 @@ const Game = () => {
 					<div className={styles.content__header}>header</div>
 					<div className={styles.content__main}>
 						<div className={styles.main__card}>card</div>
-						<div className={styles.main__deck}>deck</div>
+						<div className={styles.main__deck}>
+							<Deck_blue/>
+							<Deck_red/>
+							<Deck_green/>
+							<Deck_yellow/>
+						</div>
 					</div>
 					<div className={styles.content__buttons}>buttons</div>
 				</div>
-				<div className={styles.game__chat}>chat</div>
+				{/*<div className={styles.game__chat}>chat</div>*/}
 			</div>
 			<div className={styles.game__right}> </div>
 		</div>
---
+++ src/main/ui/components/game/deck/Deck_blue.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './Deck_blue.module.css';
+import img1 from '../../../images/shirt 1.png'
+
+
+const Deck_blue = () => {
+
+	return (
+		<div className={styles.deck__wrap}>
+			<div className={styles.deck}>
+				<div className={styles.card__imgBx}>
+					<img src={img1} alt="images"/>
+				</div>
+				<div className={styles.deck__details}>
+					<h2> Famous</h2>
+				</div>
+			</div>
+		</div>
+	)
+}
+export default Deck_blue;
