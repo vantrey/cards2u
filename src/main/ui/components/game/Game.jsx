1602195ec889975852e3e1462b986e43f884d1d6
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2020-06-26 15:47:41 +0400

    games -> added decs block + css

--- src/main/ui/components/game/Game.jsx
+++ src/main/ui/components/game/Game.jsx
@@ -4,7 +4,7 @@ import Deck_blue from "./deck/Deck_blue";
 import Deck_red from "./deck/Deck_red";
 import Deck_green from "./deck/Deck_green";
 import Deck_yellow from "./deck/Deck_yellow";
-
+import ava from '../../images/shirt6.png';
 
 
 const Game = () => {
@@ -15,14 +15,28 @@ const Game = () => {
 			<div className={styles.game__container}>
 				<div className={styles.game__analytics}>analytics</div>
 				<div className={styles.game__content}>
-					<div className={styles.content__header}>header</div>
+					<div className={styles.content__header}>
+						<div className={styles.header__avatar}>
+							<img src={ava} alt="avatar"/>
+						</div>
+						<div className={styles.header__info}>
+							<h3 className={styles.header__title}>your favorite decks</h3>
+							<div className={styles.header__decks}>
+								<div className={`${styles.decks__item} ${styles.decks__item_1}`}></div>
+								<div className={`${styles.decks__item} ${styles.decks__item_2}`}></div>
+								<div className={`${styles.decks__item} ${styles.decks__item_3}`}></div>
+								<div className={`${styles.decks__item} ${styles.decks__item_4}`}></div>
+								<div className={`${styles.decks__item} ${styles.decks__item_5}`}></div>
+							</div>
+						</div>
+					</div>
 					<div className={styles.content__main}>
 						<div className={styles.main__card}>card</div>
 						<div className={styles.main__deck}>
 							<Deck_blue/>
-							<Deck_red/>
-							<Deck_green/>
-							<Deck_yellow/>
+							{/*<Deck_red/>*/}
+							{/*<Deck_green/>*/}
+							{/*<Deck_yellow/>*/}
 						</div>
 					</div>
 					<div className={styles.content__buttons}>buttons</div>
