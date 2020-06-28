80d3146774f43134e4ed7f354933e121bf581c43
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2020-06-28 16:27:34 +0400

    games -> card-back added animation

--- src/main/ui/components/game/card/CardDownside.jsx
+++ src/main/ui/components/game/card/CardDownside.jsx
@@ -8,6 +8,18 @@ const CardDownside = () => {
 		<div className={styles.card__wrap}>
 			<div className={styles.card}>
 
+				<div  className={styles.card__buttons}>
+					<div  className={styles.buttons__group}>
+						<button >0%</button>
+						<button >25%</button>
+						<button >75%</button>
+						<button >100%</button>
+					</div>
+				</div>
+
+
+
+
 
 			</div>
 		</div>
