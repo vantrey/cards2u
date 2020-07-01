4fd17cd5128c05f72aba1d19fda8aa27ebaa003a
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2020-07-01 16:13:44 +0400

    profile -> added main styles

--- src/main/ui/components/profile/ui/ProfileContainer.tsx
+++ src/main/ui/components/profile/ui/ProfileContainer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './Profile.module.css'
-import { useSelector} from "react-redux";
+import {useSelector} from "react-redux";
 import {AppStateType} from "../../../../bll/store/store";
 import ProfileAvatarContainer from "./profileAvatar/ProfileAvatarContainer";
 import ProfileInfoContainer from './profileInfo/ProfileInfoContainer';
@@ -14,21 +14,29 @@ const ProfileContainer = () => {
     return (
         <div className={styles.profile__wrap}>
             <div className={styles.profile__main}>
-
-                <div className={styles.main__body}>
-
-                    <ProfileAvatarContainer
-                        user={user}
-                        isFetching={isFetching}
-                    />
-
-                    <ProfileInfoContainer
-                        user={user}
-                        isFetching={isFetching}
-                    />
-
-                    <FeedbackContainer/>
-
+                <h5 className={styles.main__title}>Personal Area</h5>
+                <div className={styles.main__wrap}>
+                    <div className={styles.main__user}>
+                        <div className={styles.user__avatar}>
+                            <ProfileAvatarContainer
+                                user={user}
+                                isFetching={isFetching}
+                            />
+                        </div>
+                        <div className={styles.user__nick}>
+                            <ProfileInfoContainer
+                                user={user}
+                                isFetching={isFetching}
+                            />
+                        </div>
+                        <div className={styles.user__feedback}>
+                            <FeedbackContainer/>
+                        </div>
+                    </div>
+                    <div className={styles.main__rest}>
+                        <div className={styles.rest__decks}></div>
+                        <div className={styles.rest__chat}></div>
+                    </div>
                 </div>
             </div>
         </div>
