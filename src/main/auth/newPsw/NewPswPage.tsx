2aac8cd2d837f5a7b9c222e4aeab1aedd0cfdc47
natasha17-spec <17061990@inbox.ru>
2020-07-04 23:28:00 +0300

    delete file Natali

--- src/main/auth/newPsw/NewPswPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import NewPswContainer from "./NewPswContainer";
-
-const NewPswPage: React.FC = () => {
-  return <NewPswContainer/>
-}
-
-export default NewPswPage
--- src/main/auth/restorePsw/RestorePswPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import RestorePswContainer from "./RestorePswContainer";
-
-
-const RestorePswPage: React.FC = () => {
-    return <RestorePswContainer/>
-};
-
-export default RestorePswPage;
