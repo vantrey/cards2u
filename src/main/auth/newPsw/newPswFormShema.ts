3adf4b47e734ef9720a7074f718ef09cb314b94a
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2020-07-04 21:22:26 +0400

    delete src +pjson

--- src/main/auth/newPsw/newPswFormShema.ts
+++
@@ -1,8 +0,0 @@
-import * as yup from "yup";
-
-export const newPswFormSchema = yup.object().shape({
-  password: yup.string().required('⚠ please, fill up your password')
-    .min(8, `password has to be at least ${8} characters long.`),
-  passwordConfirmation: yup.string()
-    .oneOf([yup.ref('password'), undefined], 'password mismatch')
-})
--- src/main/ui/components/profile/ui/feedBack/feedBackFormSchema.ts
+++
@@ -1,8 +0,0 @@
-import * as yup from "yup";
-
-export const feedbackFormSchema = yup.object().shape({
-    name: yup.string().required('⚠ please, fill up your name'),
-    email: yup.string().required('⚠ please, fill up your email')
-        .email('⚠ please, fill up a valid email address'),
-    message: yup.string().required('⚠ please, enter your message')
-});
