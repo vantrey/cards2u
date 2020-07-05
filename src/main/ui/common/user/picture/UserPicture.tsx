c53c22b35681a2c8f4b2dc61b3e04a492e9c8cc0
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2020-07-06 02:26:09 +0400

    find -> game -> add default ava +nick

--- src/main/ui/common/user/picture/UserPicture.tsx
+++ src/main/ui/common/user/picture/UserPicture.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './UserPicture.module.css';
+import avaDefault from "../../../images/ava-default.png";
 
 type AvaPictureTypeProps={
     avatar:string,
@@ -11,10 +12,24 @@ const UserPicture: React.FC<AvaPictureTypeProps> = ({avatar,nick}) => {
     return (
         <div className={styles.picture__wrap}>
             <div className={styles.picture__avatar}>
-                <img src={avatar} alt='avatar'/>
+                {
+                    avatar &&
+					<img src={avatar} alt='avatar'/>
+                }
+                {
+                    !avatar &&
+					<img src={avaDefault} alt='avatar'/>
+                }
             </div>
             <div className={styles.picture__nick}>
-                {nick}
+                {
+                    nick &&
+					<span>{nick}</span>
+                }
+                {
+                    !nick &&
+					<span>john wick</span>
+                }
             </div>
         </div>
     );
