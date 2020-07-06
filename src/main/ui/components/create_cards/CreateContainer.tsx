a5e2b2361aac235efe62038bd432e07486a586ea
Ivan Tolkachev <vantrey@yandex.ru>
2020-07-06 11:19:46 +0300

    create cards-> add CreateContainer.tsx, cardEntersForms.tsx

--- src/main/ui/components/create_cards/CreateContainer.tsx
+++ src/main/ui/components/create_cards/CreateContainer.tsx
@@ -1,28 +1,20 @@
 import React from 'react';
 import styles from './Create.module.css';
-import {profileActions} from "../../../bll/profile/profileReducer";
 import {useDispatch} from "react-redux";
-
-
+import CardEntersContainer from "./cardEnters/CardEntersContainer";
+import UserCardsContainer from "./userCards/UserCardsContainer";
 
 const CreateContainer = () => {
 
 	const dispatch = useDispatch();
 
-	dispatch(profileActions.setUser({
-		avatar: '',
-		created: '',
-		email: '',
-		isAdmin: false,
-		name: '',
-		publicCardPacksCount: 0,
-		updated: '',
-		verified: false,
-		_id: ''}));
 
 	return (
 		<div className={styles.create__wrap}>
 
+			<CardEntersContainer/>
+
+			<UserCardsContainer/>
 
 
 		</div>
---
+++ src/main/ui/components/create_cards/userCards/UserCards.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from '../Create.module.css'
+
+type PropsType = {}
+
+
+const UserCards: React.FC<PropsType> = React.memo(({}) => {
+
+    return (
+        <div>
+
+
+
+        </div>
+    )
+});
+export default UserCards;
