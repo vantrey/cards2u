daea0b96e5fe3f1732bdf7565272a8726054ea91
Ivan Tolkachev <vantrey@yandex.ru>
2020-07-10 10:50:51 +0300

    fix bug in features for start app

--- src/main/features/Cards/ui/CardsContainer.tsx
+++ src/main/features/Cards/ui/CardsContainer.tsx
@@ -41,10 +41,10 @@ const CardsContainer: React.FC = () => {
         dispatch(add_Card({cardsPack_id: pack_id, question: valueQuestion, answer: valueAnswer}))
     }
     const onDeleteCard = (card_id: string) => {
-        dispatch(delete_Card(card_id, pack_id))
+        dispatch(delete_Card(card_id, /*pack_id*/))
     }
     const onUpdateCard = (_id: string, question: string, answer: string) => {
-        dispatch(update_Card({_id, question, answer}, pack_id))
+        dispatch(update_Card({_id, question, answer}, /*pack_id*/))
     }
 
     const pageCountSize = Math.ceil(cardsTotalCount / pageCount)
