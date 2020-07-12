c819fad73925aed378c2b96553f6c7bc705693e7
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2020-07-12 14:03:59 +0400

    added fonts for cyrillic

--- src/main/ui/components/game/card/Card.jsx
+++ src/main/ui/components/game/card/Card.jsx
@@ -13,8 +13,8 @@ let Card = ({ cardBg, setCardFace, cardface }) => {
 			<div className={styles.card}>
 				<div className={styles.card__background} style={stylesOnline}> </div>
 				<div className={styles.card__content}>
-					<h2 className={styles.card__title}>Mountain View</h2>
-					<p className={styles.card__text}>Check out all of these gorgeous mountain trips with beautiful
+					<h2 className={styles.card__title}>React Native</h2>
+					<p className={styles.card__text}> Что то русскими буквами Что то русскими буквами Что то русскими буквамиCheck out all of these gorgeous mountain trips with beautiful
 						views of, you guessed it, the mountains</p>
 					<button className={styles.card__btn} onClick={() => {setCardFace (!cardface)}}>see answer</button>
 				</div>
