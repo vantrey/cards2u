96ff86eba29d58a4a6273eae4a668170113708fc
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2020-07-14 16:35:44 +0400

    main -> change intro

--- src/main/ui/components/intro/Intro.jsx
+++ src/main/ui/components/intro/Intro.jsx
@@ -3,9 +3,9 @@ import styles from './Intro.module.css'
 import soundOf from '../../icons/sound-of.svg'
 import soundOn from '../../icons/sound-on.svg'
 import skip from '../../icons/skip-circle .svg'
-import intro from '../../video/Intro-53-commpres.mp4'
+import intro from '../../video/newIntro.mp4'
 import intro_webm from '../../video/Intro-53-commpres-webm.webm'
-import poster from '../../images/main-bg.webp'
+import poster from '../../images/main-bg.png'
 
 
 const Intro = ({ setBg, toggleBg }) => {
