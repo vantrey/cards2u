33a57cb10b4e534bd4d2048a3d3d45d28ef8ce7c
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2020-07-21 03:55:09 +0400

    CreateCardTexarea -> added tooltip

--- src/main/ui/common/createCardTextarea/CreateCardTextarea.tsx
+++ src/main/ui/common/createCardTextarea/CreateCardTextarea.tsx
@@ -22,29 +22,35 @@ const CreateCardTextarea: React.FC<PropsType> = ({
                                                  }) => {
 
     return (
-        <div className={styles.form__item}>
+        < div className={styles.form__wrap} >
+            <div className={styles.form__item} tabIndex={1} >
             <textarea
                 className={styles.form__textarea}
                 ref={register}
                 {...props}
                 name={name}
             />
+
+                {/*<div style={{fontFamily: 'arial'}}>{restLimit}</div>*/}
+
+                {errors[name] &&
+
+				<div className={styles.form__warning_wrap}>
+					<div className={styles.form__warning_icon}>
+						<img src={warning} alt="warning"/>
+					</div>
+					<span className={styles.form__warning_info}>{errors[name].message}</span>
+				</div>
+                }
+            </div>
             <div className={styles.tooltip}>
                 <div className={styles.tooltip_wrap}>
-                    <h4 className={styles.tooltip_title}>123</h4>
+                    <h4 className={styles.tooltip_title}>123 df dgd
+                        gdfgd dsfgdfgd dg dg dfsdfsdf sdfsdfsd
+                    </h4>
                 </div>
+                <div className={styles.tooltip__arrow}></div>
             </div>
-            <div style={{fontFamily: 'arial'}}>{restLimit}</div>
-
-            {errors[name] &&
-
-            <div className={styles.form__warning_wrap}>
-                <div className={styles.form__warning_icon}>
-                    <img src={warning} alt="warning"/>
-                </div>
-                <span className={styles.form__warning_info}>{errors[name].message}</span>
-            </div>
-            }
         </div>
     )
 };
