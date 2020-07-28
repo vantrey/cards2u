c3072fc1a7f6ef9c35a0af90659475b2ff600c82
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2020-07-28 20:05:29 +0400

    Switcher.jsx -> + tooltip

--- src/main/ui/components/game/analytics/switchers/Switcher.jsx
+++ src/main/ui/components/game/analytics/switchers/Switcher.jsx
@@ -9,6 +9,11 @@ const Switcher = () => {
 			<span className={`${styles.switcher} ${styles.switcher1}`}>
    				<input className={styles.switcher__input} type="checkbox" id="switcher-1" />
    				<label className={styles.switcher__label} htmlFor="switcher-1"> </label>
+				<div className={styles.tooltip}>
+				<div className={styles.tooltip_wrap}>
+					<h4 className={styles.tooltip_title}> еусыа ывапыаы впвапв </h4>
+				</div>
+			</div>
 			</span>
 		</div>
 	)
