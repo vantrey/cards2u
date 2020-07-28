d22951ac63acdc49c13d3e1886c077592cc0f1ce
Ivan Tolkachev <vantrey@yandex.ru>
2020-07-28 20:32:59 +0300

    getCard.ts -> 5 grades instead 6

--- src/main/helpers/getCard/getCard.ts
+++ src/main/helpers/getCard/getCard.ts
@@ -1,10 +1,10 @@
 import {CardType} from "../../types/entities";
 
 export const getControlRandomCard = (cards: Array<CardType>) => {
-    const sum = cards.reduce((acc, card) => acc + (6 - card.grade) * (6 - card.grade), 0);
+    const sum = cards.reduce((acc, card) => acc + (5 - card.grade) * (5 - card.grade), 0);
     const rand = Math.random() * sum;
     const res = cards.reduce((acc: { sum: number, id: number }, card, i) => {
-            const newSum = acc.sum + (6 - card.grade) * (6 - card.grade);
+            const newSum = acc.sum + (5 - card.grade) * (5 - card.grade);
             return {sum: newSum, id: newSum < rand ? i : acc.id}
         }
         , {sum: 0, id: -1});
